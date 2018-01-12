2da39561578f5c8fd205a88d0399644e04402249
Christoffer Nilsson <chriszmn@gmail.com>
2018-01-12 16:16:44 +0100

    test

--- app/src/main/java/com/project/iotap/iotap/Mqtt/MqttConstants.java
+++ app/src/main/java/com/project/iotap/iotap/Mqtt/MqttConstants.java
@@ -5,10 +5,10 @@ package com.project.iotap.iotap.Mqtt;
  */
 public class MqttConstants {
 
-    public static final String MQTT_BROKER_URL = "tcp://m23.cloudmqtt.com:16532";
+    public static final String MQTT_BROKER_URL = "tcp://m23.cloudmqtt.com:15915";
     public static final String CLIENT_ID = "android";
-    public static final String CLIENT_USER = "enqeeoyx";
-    public static final String CLIENT_PASSWORD = "uBY_2qW7Eesw";
+    public static final String CLIENT_USER = "hlmtgqdu";
+    public static final String CLIENT_PASSWORD = "ZbyyNuAPignO";
     public static final String GREETING_TOPIC = "Greeting";
     public static final String GREETING = "1:";
     public static final String COMMAND = "2:";
